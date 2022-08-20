25a2ec2f780c565ec87236fe28432dcc105aa5ef
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-08-20 18:07:40 +0200

    modif scss header

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import fb from '../../assets/images/fb.svg';
+import insta from '../../assets/images/insta.svg';
+import yt from '../../assets/images/yt.svg';
+import twitch from '../../assets/images/twitch.svg';
+import discord from '../../assets/images/discord.svg';
 
 const Navigation = () => {
     return (
         <nav>
-            <div className="icon"></div>
+            <div className="navicon">
+                <li>
+                    <img src={fb} alt="facebook" />
+                </li>
+                <li>
+                    <img src={insta} alt="instagram" />
+                </li>
+                <li>
+                    <img src={yt} alt="youtube" />
+                </li>
+                <li>
+                    <img src={twitch} alt="twitch" />
+                </li>
+                <li>
+                    <img src={discord} alt="discord" />
+                </li>
+            </div>
             <ul>
                 <li>
                     <NavLink to="/">
